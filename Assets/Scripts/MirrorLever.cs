44080c7524585090c01baa7ce5a8b09df560e36c
mobinasarhangi <mobinasarhangi365@gmail.com>
2026-07-05 11:23:05 +0330

    Add world mirror mechanic with lever and light/dark platform toggling

---
+++ Assets/Scripts/MirrorLever.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+// این اسکریپت رو روی یک Trigger (اهرم/دکمه) بچسبون
+// نیاز به Box Collider 2D با تیک "Is Trigger" فعال
+public class MirrorLever : MonoBehaviour
+{
+    [Tooltip("فقط این لایه بتونه اهرم رو فعال کنه (معمولا Player)")]
+    public LayerMask activatorLayer;
+
+    private WorldMirrorManager mirrorManager;
+    private SpriteRenderer spriteRenderer;
+    public Color activeColor = Color.cyan;
+    public Color inactiveColor = Color.gray;
+
+    void Start()
+    {
+        mirrorManager = FindObjectOfType<WorldMirrorManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & activatorLayer) != 0)
+        {
+            if (mirrorManager != null)
+            {
+                mirrorManager.ToggleMirror();
+                if (spriteRenderer != null)
+                {
+                    spriteRenderer.color = mirrorManager.isMirrored ? activeColor : inactiveColor;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WorldMirrorManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+// این اسکریپت رو روی همون آبجکت "GameManager" بچسبون
+public class WorldMirrorManager : MonoBehaviour
+{
+    [Header("Light World Objects")]
+    [Tooltip("اشیایی که فقط توی دنیای نور (حالت عادی) وجود دارن")]
+    public GameObject[] lightWorldObjects;
+
+    [Header("Dark World Objects")]
+    [Tooltip("اشیایی که فقط توی دنیای معکوس (تاریکی) وجود دارن")]
+    public GameObject[] darkWorldObjects;
+
+    [Header("Status (Read Only)")]
+    public bool isMirrored = false;
+
+    void Start()
+    {
+        ApplyState();
+    }
+
+    public void ToggleMirror()
+    {
+        isMirrored = !isMirrored;
+        ApplyState();
+    }
+
+    void ApplyState()
+    {
+        // اگه معکوس شده، دنیای تاریکی رو فعال کن و دنیای نور رو خاموش کن، و برعکس
+        foreach (GameObject obj in lightWorldObjects)
+        {
+            if (obj != null) obj.SetActive(!isMirrored);
+        }
+
+        foreach (GameObject obj in darkWorldObjects)
+        {
+            if (obj != null) obj.SetActive(isMirrored);
+        }
+    }
+}
